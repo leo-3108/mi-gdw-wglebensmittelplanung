1dfd62a4c8c4f334207c8016e5ae30f5bedf1785
Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>
2019-10-21 16:44:00 +0200

    update Aufgaben AB1

--- _arbeitsblaetter/Pelzer/grundlagen_javascript4.js
+++ _arbeitsblaetter/Pelzer/grundlagen_javascript4.js
@@ -1,26 +1,35 @@
 //Aufgabe 4
-
 const readline = require('readline');
 const rl = readline.createInterface(process.stdin,process.stdout);
 
-let bewertung = 0
-maxBewertung = 5
+let bewertung = 0;
+maxBewertung = 5;
 
-rl.question('Wieviele Bewertungen?: ', function(anzahlBewertungen) {
-
+//Eingabe der Anzahl der Bewertungen
+rl.question('Anzahl der Bewertungen: ', function(anzahlBewertungen){
+  //Funktion zu Erstellung einer Zufallszahl
   const getRandom = function(min, max) {
       return Math.round(Math.random() * (max - min) + min);
-  }
-
+}
+if(anzahlBewertungen>0){
   for (let i = 0; i < anzahlBewertungen; i++){
 
-    let neueBewertung = getRandom(0, maxBewertung)
-
+    //Berechnung der neuen Bewertung
+    let neueBewertung = getRandom(0, maxBewertung);
     bewertung = (bewertung*i+neueBewertung)/(i+1);
-    console.log('Bewertung:')
-    console.log(bewertung)
 
+    //Ausgabe
+    console.log('abgegebene Bewertung:');
+    console.log(neueBewertung);
+    console.log('aktuelle Bewertung:');
+    console.log(bewertung);
+    console.log('Anzahl der Bewertungen:');
+    console.log(i+1);
+    console.log('------------------');
   }
-
+}
+else{
+  console.log('Fehler')
+}
 rl.close();
 });
--- _arbeitsblaetter/Pelzer/grundlagen_javascript5.js
+++ _arbeitsblaetter/Pelzer/grundlagen_javascript5.js
@@ -1,31 +1,43 @@
 //Aufgabe 5
-
 const readline = require('readline');
 const rl = readline.createInterface(process.stdin,process.stdout);
 
-let bewertung = 0
-maxBewertung = 5
+let bewertung = 0;
+maxBewertung = 5;
 
-bewertung_berechnen()
-
-const bewertung_berechnen = function(){
-
-rl.question('Wieviele Bewertungen?: ', function(anzahlBewertungen) {
-
+//Eingabe der Anzahl der Bewertungen
+rl.question('Anzahl der Bewertungen: ', function(anzahlBewertungen){
+  //Funktion zu Erstellung einer Zufallszahl
   const getRandom = function(min, max) {
       return Math.round(Math.random() * (max - min) + min);
-  }
-
-  for (let i = 0; i < anzahlBewertungen; i++){
-
-    let neueBewertung = getRandom(0, maxBewertung)
-
-    bewertung = (bewertung*i+neueBewertung)/(i+1);
-    console.log('Bewertung:')
-    console.log(bewertung)
-
-  }
 }
+
+//Berechnung der Bewertung ist eine Funktion
+const berechneBewertung = function(anzahlBewertungen){
+  if(anzahlBewertungen>0){
+    for (let i = 0; i < anzahlBewertungen; i++){
+
+      //Berechnung der neuen Bewertung
+      let neueBewertung = getRandom(0, maxBewertung);
+      bewertung = (bewertung*i+neueBewertung)/(i+1);
+
+      //Ausgabe
+      console.log('abgegebene Bewertung:');
+      console.log(neueBewertung);
+      console.log('aktuelle Bewertung:');
+      console.log(bewertung);
+      console.log('Anzahl der Bewertungen:');
+      console.log(i+1);
+      console.log('------------------');
+      }
+    }
+    else{
+      console.log('Fehler')
+    }
+}
+
+//Funktion wird ausgeführt
+berechneBewertung(anzahlBewertungen);
 
 rl.close();
 });
