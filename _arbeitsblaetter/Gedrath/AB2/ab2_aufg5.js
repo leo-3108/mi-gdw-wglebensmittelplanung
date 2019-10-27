64cc60660b5031c29ecc004073ef794b5aec008f
Finn Nils Gedrath <finn_nils.gedrath@smail.th-koeln.de>
2019-10-27 21:31:41 +0100

    AB 2/5

---
+++ _arbeitsblaetter/Gedrath/AB2/ab2_aufg5.js
@@ -0,0 +1,43 @@
+/**
+ * TH Koeln - Campus Gummersbach
+ * Grundlagen des Web (Medieninformatik Ba.)
+ *
+ * @author Finn Nils Gedrath
+ * @arbeitsblatt 2 / 3
+ */
+
+/**
+ * Funktioniert nicht, da auf world nur lokal in helloWorld() zugegriffen werden kann und diese Variable nicht in worldHello() existiert.
+
+const hello = "hello";
+
+function helloWorld(){
+    const world = "World";
+
+    console.log(hello + " " + world);
+
+}
+
+function worldHello(){
+    console.log(world + " " + hello);
+}
+
+
+helloWorld();
+worldHello();
+*/
+const hello = "hello";
+const world = "World";
+
+function helloWorld(){
+    console.log(hello + " " + world);
+
+}
+
+function worldHello(){
+    console.log(world + " " + hello);
+}
+
+
+helloWorld();
+worldHello();
