05f10cbb250365ef27c127324a8b030145b0eb39
Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>
2019-10-27 22:30:34 +0100

    update Aufgaben AB2.1

--- _arbeitsblaetter/Pelzer/arbeitsblatt2/arbeitsblatt2_aufgabe1.js
+++ _arbeitsblaetter/Pelzer/arbeitsblatt2/arbeitsblatt2_aufgabe1.js
@@ -1,18 +1,36 @@
 // Aufgabe 1
 const readline = require('readline');
-const rl1 = readline.createInterface(process.stdin,process.stdout);
-const rl2 = readline.createInterface(process.stdin,process.stdout);
+const rl = readline.createInterface(process.stdin,process.stdout);
 
-let bewertung = 0;
+let bewertung = ['Bewertung', 0, 0];
+const maxBewertung = 5;
+
+//Funktion zu Erstellung einer Zufallszahl
+const getRandom = function(min, max) {
+      return Math.round(Math.random() * (max - min) + min);
+}
+
+//Berechnung der Bewertung
+const berechneBewertung = function(anzahlBewertungen, tmp, neueBewertung){
+      bewertung = (anzahlBewertungen*tmp+neueBewertung)/(anzahlBewertungen)
+
+      //Ausgabe
+      console.log('abgegebene Bewertung:');
+      console.log(neueBewertung);
+      console.log('aktuelle Bewertung:');
+      console.log(tmp);
+      console.log('Anzahl der Bewertungen:');
+      console.log(anzahlBewertungen);
+      console.log('------------------');
+
+      return bewertung
+}
 
 //Eingabe der Anzahl der Bewertungen
-rl1.question('Anzahl der Bewertungen: ', function(anzahlBewertungen){
+rl.question('Anzahl der Bewertungen: ', function(anzahlBewertungen){
+    for (let i = 0; i < anzahlBewertungen; i++){
+      bewertung[2] = berechneBewertung(bewertung[1]++, bewertung[2], getRandom(0, maxBewertung))
+    }
 
-for (let i = 0; i < anzahlBewertungen; i++){
-  rl2.question('Bewertung eingeben: ', function(tmp){
-    bewertung.push(tmp)
-  }
-rl2.close();
-});
-rl1.close();
+rl.close();
 });
