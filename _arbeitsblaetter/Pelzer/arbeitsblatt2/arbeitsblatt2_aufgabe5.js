35bc3222a28a2f730594765ed61ed8d6c877b088
Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>
2019-10-30 14:40:43 +0100

    update AB2.5

--- _arbeitsblaetter/Pelzer/arbeitsblatt2/arbeitsblatt2_aufgabe5.js
+++ _arbeitsblaetter/Pelzer/arbeitsblatt2/arbeitsblatt2_aufgabe5.js
@@ -1,1 +1,14 @@
 // Aufgabe 5
+const hello = "Hello"
+
+function konkatenieren1(){
+  const world = "World";
+  console.log(hello+world);
+  }
+
+  function konkatenieren2(){  //Funktion kann nicht auf world zugreifen, da diese lokal in "konkatenieren1" existiert
+    console.log(world+hello);
+    }
+
+  konkatenieren1();
+  //konkatenieren2();
