8ca92b10638c7d03e8623d5a82cdefff408604c0
Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>
2019-11-01 13:36:55 +0100

    update Aufgabe AB3.2

--- _arbeitsblaetter/Pelzer/arbeitsblatt3/arbeitsblatt3_aufgabe2.js
+++ _arbeitsblaetter/Pelzer/arbeitsblatt3/arbeitsblatt3_aufgabe2.js
@@ -1,1 +1,11 @@
 // Aufgabe 2
+var fs = require('fs');
+
+let data = fs.readFile('cities.json', (err, data)=>{
+  if(err){
+    console.log("Fehler");
+  }
+  else{
+    console.log(data.toString());
+  }
+});
