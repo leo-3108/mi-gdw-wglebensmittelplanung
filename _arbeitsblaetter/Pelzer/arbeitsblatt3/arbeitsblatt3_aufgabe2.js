536b3bdd6aa59d23d624eea930bf89c93767cd70
Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>
2019-11-02 12:58:34 +0100

    update Aufgabe AB3.2+cities

--- _arbeitsblaetter/Pelzer/arbeitsblatt3/arbeitsblatt3_aufgabe2.js
+++ _arbeitsblaetter/Pelzer/arbeitsblatt3/arbeitsblatt3_aufgabe2.js
@@ -1,5 +1,7 @@
 // Aufgabe 2
 var fs = require('fs');
+const readline = require('readline');
+const rl = readline.createInterface(process.stdin,process.stdout);
 
 let data = fs.readFile('cities.json', (err, data)=>{
   if(err){
@@ -7,6 +9,23 @@ let data = fs.readFile('cities.json', (err, data)=>{
   }
   else{
     console.log(data.toString());
-    let dataArray = JSON.parse(data)
+    var dataArray = JSON.parse(data)
+
+    function search(dataArray){
+      rl.question('Suchbegriff: ',function search(suchbegriff){
+        for(let i = 0; i < 10; i++){
+        if (suchbegriff==dataArray.cities[i].name){
+          console.log("Gefunden");
+          break;
+        }
+        else{
+          console.log("...")
+        }
+      }
+      });
+    }
+
+    search(dataArray);
+
   }
 });
