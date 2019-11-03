a33ccf2bef6c6eb71bb0b25f8e2a04475bd4b1ae
Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>
2019-11-03 12:36:50 +0100

    add Aufgabe AB3.3

---
+++ _arbeitsblaetter/Pelzer/arbeitsblatt3/arbeitsblatt3_aufgabe3.js
@@ -0,0 +1,46 @@
+// Aufgabe 3
+const m = require('./modul.js')
+var fs = require('fs');
+const readline = require('readline');
+const rl = readline.createInterface(process.stdin,process.stdout);
+
+let data = fs.readFile('cities.json', (err, data)=>{
+  if(err){
+    console.log("Fehler");
+  }
+  else{
+    var dataArray = JSON.parse(data);
+
+function auswaehlen(){
+rl.question('0: ausgeben; 1: suchen, 2: löschen, 3: hinzufügen 4: beenden -->',function suchen(befehl){
+  if(befehl==0){
+    console.log(dataArray);
+    auswaehlen();
+  }
+  if(befehl==1){
+    rl.question('Suchbegriff: ',function suchen(suchbegriff){
+        if(m.search(dataArray, suchbegriff)==true){
+          console.log("Name gefunden");
+        }
+        else{
+          console.log("Name nicht gefunden");
+        }
+            auswaehlen();
+    });
+  }
+  if(befehl==2){
+    rl.question('Suchbegriff: ',function suchen(begriff){
+        m.loeschen(dataArray, begriff)
+            auswaehlen();
+    });
+  }
+  if(befehl==4){
+    console.log(dataArray);
+  }
+  });
+}
+
+auswaehlen();
+//console.log("test")
+  }
+});
