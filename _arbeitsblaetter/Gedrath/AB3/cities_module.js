c52ac29504b33dcffe08b1632769888628119653
Finn Nils Gedrath <finn_nils.gedrath@smail.th-koeln.de>
2019-11-03 20:30:10 +0100

    Add AB3/2 - add

--- _arbeitsblaetter/Gedrath/AB3/cities_module.js
+++ _arbeitsblaetter/Gedrath/AB3/cities_module.js
@@ -1,8 +1,8 @@
 /**
- * Sucht das Objekt der Stadt anhand des Namens
- * @param  {Objekt-Array} dataArray     Die ursprüngliche Liste an Städten
- * @param  {String} name                Der zusuchende Namens
- * @return {Objekt}                     Das Objekt mit der Stadt
+ * Sucht das Objekt der Stadt anhand des Namens.
+ * @param  {Objekt-Array} dataArray     Die ursprüngliche Liste an Städten.
+ * @param  {String} name                Der zusuchende Namens.
+ * @return {Objekt|false}               Das Objekt mit der Stadt.
  */
 exports.search = (dataArray, name) => {
     dataArray.forEach(function(item, index, array){
@@ -14,6 +14,12 @@ exports.search = (dataArray, name) => {
     return false
 }
 
+/**
+ * Sucht das Objekt der Stadt anhand des Namens und löscht dieses.
+ * @param  {Objekt-Array} dataArray     Die ursprüngliche Liste an Städten.
+ * @param  {String} name                Der zusuchende Namens.
+ * @return {Array|false}                Das neue Array.
+ */
 exports.delete = (dataArray, name) => {
     dataArray.forEach(function(item, index, array){
         if(item.stadt_name == name){
@@ -23,4 +29,21 @@ exports.delete = (dataArray, name) => {
     });
 
     return false
+}
+
+/**
+ * Fügt ein neues Element hinzu.
+ * @param {Objekt-Array} dataArray      Die ursprüngliche Liste an Städten.
+ * @param {String} stadt_name           Name der Stadt.
+ * @param {Int} einwohner               Anzahl an Einwohnern
+ * @param {String} bundesland           Name des Bundesland.
+ */
+exports.add = (dataArray, stadt_name, einwohner, bundesland) => {
+    dataArray.push({
+        "stadt_name":   stadt_name,
+        "einwohner":    einwohner,
+        "bundesland":   bundesland
+    });
+
+    return dataArray
 }
