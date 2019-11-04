005e214af22a4d9ae1058b6f5c0f50e844a1c7cd
Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>
2019-11-05 00:15:48 +0100

    update modul.js

--- _arbeitsblaetter/Pelzer/arbeitsblatt3/modul.js
+++ _arbeitsblaetter/Pelzer/arbeitsblatt3/modul.js
@@ -1,6 +1,6 @@
 exports.search = function(dataArray, begriff){
     for(let i = 0; i < dataArray.cities.length; i++){
-    if (begriff==dataArray.cities[i].name){
+    if (begriff==dataArray.cities[i].stadtname){
       return true;
     }
   }
@@ -11,7 +11,7 @@ exports.loeschen = function(dataArray, name){
   if(this.search(dataArray, name)==true)
 
   for(let i = 0; i < dataArray.cities.length; i++){
-  if (name==dataArray.cities[i].name){
+  if (name==dataArray.cities[i].stadtname){
     delete dataArray.cities[i];
     console.log("deleted")
     }
