99d889e08ae7063fdf22c269e60ec2c766849a20
Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>
2019-11-18 12:21:38 +0100

    update Aufgabe AB3.4
    Asynchrone Funktion verbessert

--- _arbeitsblaetter/Pelzer/arbeitsblatt3/arbeitsblatt3_aufgabe4.js
+++ _arbeitsblaetter/Pelzer/arbeitsblatt3/arbeitsblatt3_aufgabe4.js
@@ -4,39 +4,42 @@ var fs = require('fs');
 const readline = require('readline');
 const rl = readline.createInterface(process.stdin,process.stdout);
 
-let cityData = fs.readFile('cities.json', (err, cityData)=>{
-  if(err){
-    console.log("Fehler");
+async function zusammenfassen() {
+  const getJSON = (file) => {
+      return new Promise((resolve, reject) => {
+          fs.readFile(file, (error, data) => {
+              if(error) reject(error);
+              resolve(JSON.parse(data));
+          });
+      });
   }
-  else{
-    var cityData = JSON.parse(cityData);
+  try{
+    let cityData = await getJSON('./cities.json');
+    let userData = await getJSON('./user.json');
 
-    let userData = fs.readFile('user.json', (err, userData)=>{
-      if(err){
-        console.log("Fehler");
+
+  //zusammenfassen
+  let neu = [];
+  for(let i = 0; i < userData.user.length; i++){
+    for(let o = 0; o < cityData.cities.length; o++){
+      if (userData.user[i].wohnort == cityData.cities[o].stadtname){
+        neu.push(Object.assign(userData.user[i],cityData.cities[o]));
       }
-      else{
-        var userData = JSON.parse(userData);
-
-        //Daten zusammenfügen
-
-        let neu = [];
-        for(let i = 0; i < userData.user.length; i++){
-          for(let o = 0; o < cityData.cities.length; o++){
-            if (userData.user[i].wohnort == cityData.cities[o].stadtname){
-              neu.push(Object.assign(userData.user[i],cityData.cities[o]));
-            }
-          }
-        }
-
-        for(let i = 0; i < neu.length; i++){
-        console.log(neu[i]);
-        console.log("---");
-      }
-
-
-        process.exit();
-      }
-    });
+    }
   }
-});
+
+  for(let i = 0; i < neu.length; i++){
+  console.log(neu[i]);
+  console.log("---");
+  }
+  process.exit();
+  }
+
+  catch(error){
+      console.error(error);
+      process.exit();
+    }
+
+}
+
+zusammenfassen()
