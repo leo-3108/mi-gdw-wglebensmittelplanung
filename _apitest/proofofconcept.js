081f47c3ad0d29df5da4583dbfac20270d9a7b6a
Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>
2019-12-02 17:44:54 +0100

    Update _apitest/proofofconcept.js
    Get Request; Places API

--- _apitest/proofofconcept.js
+++ _apitest/proofofconcept.js
@@ -1,10 +1,16 @@
-const request = require(‘request’);
-request('https://places.cit.api.here.com/places/v1/discover/search
-      ?app_id=hDr78BRUEK7XFKrLIm2j
-      &app_code=7eGjD8JcxivXumb6wdzvig
-      &at=52.531,13.3843
-      &q=Brandenburg+Gate', { json: true }, (err, res, body) => {
-if (err) { return console.log(err); }
-console.log(body.url);
-console.log(body.explanation);
+var request = require("request");
+
+var options = { method: 'GET',
+  url: 'https://places.cit.api.here.com/places/v1/autosuggest',
+  qs:
+   { at: '40.74917,-73.98529',
+     q: 'chrysler',
+     app_id: 'hDr78BRUEK7XFKrLIm2j',
+     app_code: '7eGjD8JcxivXumb6wdzvig' },
+  };
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
 });
