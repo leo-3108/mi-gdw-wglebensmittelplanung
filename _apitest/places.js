d8fa23598203ecd804db01b728a927ccf9eb744e
Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>
2019-12-06 11:14:25 +0100

    Update _apitest/places.js

---
+++ _apitest/places.js
@@ -0,0 +1,24 @@
+//Places API; Einfache GET-Abfrage der Supermärkte in GM
+
+const fs = require('fs')
+const request = require("request");
+
+const options = { method: 'GET',
+  url: 'https://places.cit.api.here.com/places/v1/autosuggest',
+  qs:
+   { at: '51.02496075183629,7.561652965277074',
+     q: 'Supermarkt',
+     app_id: 'hDr78BRUEK7XFKrLIm2j',
+     app_code: '7eGjD8JcxivXumb6wdzvig' },
+  };
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  fs.writeFile('./output_places.json', (body), (err) => {
+              if (err) throw err;
+              console.log('The file has been saved!');
+
+              process.exit();
+          });
+});
