d4c6103f9b715632428ce0bb692b0cb861689f56
Finn Nils Gedrath <finn_nils.gedrath@smail.th-koeln.de>
2019-12-06 15:14:46 +0100

    Updated Proof of concept

--- _apitest/README.md
+++ _apitest/README.md
@@ -1,13 +1,14 @@
 # Proof of Concept
 
-Umgebung zum Testen der Erforderlichen APIs.
+Umgebung zum Testen der erforderlichen APIs.
 
 **Verwendete APIs:**
 
 - Places API (Angeboten von der HERE REST API) - https://developer.here.com/develop/rest-apis
-  > Get global information on 150 million parks, businesses, attractions and more, including addresses, categories and contact info.
+  _Get global information on 150 million parks, businesses, attractions and more, including addresses, categories and contact info._
+
 - Routing API (Angeboten von der HERE REST API) - https://developer.here.com/develop/rest-apis
-  > Complete complex journeys more efficiently with advanced routing algorithms including truck routing, large scale matrix routing and traffic-enabled routing. Get accurate ETAs and routing instructions in over 108 languages.
+  _Complete complex journeys more efficiently with advanced routing algorithms including truck routing, large scale matrix routing and traffic-enabled routing. Get accurate ETAs and routing instructions in over 108 languages._
 
 
 ```js
--- _apitest/places.js
+++ _apitest/places.js
@@ -1,24 +1,31 @@
-//Places API; Einfache GET-Abfrage der Supermärkte in GM
+/**
+ * PROOF-OF-CONCEPT
+ *
+ * API:         Places API der HERE REST API
+ * Anfrage:     GET-Abfrage der Supermärkte in Gummersbach
+ */
 
 const fs = require('fs')
 const request = require("request");
 
-const options = { method: 'GET',
-  url: 'https://places.cit.api.here.com/places/v1/autosuggest',
-  qs:
-   { at: '51.02496075183629,7.561652965277074',
-     q: 'Supermarkt',
-     app_id: 'hDr78BRUEK7XFKrLIm2j',
-     app_code: '7eGjD8JcxivXumb6wdzvig' },
-  };
+const options = {
+    method: 'GET',
+    url: 'https://places.cit.api.here.com/places/v1/autosuggest',
+    qs: {
+        at: '51.02496075183629,7.561652965277074',
+        q: 'Supermarkt',
+        app_id: 'hDr78BRUEK7XFKrLIm2j',
+        app_code: '7eGjD8JcxivXumb6wdzvig'
+    },
+};
 
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
 
-  fs.writeFile('./output_places.json', (body), (err) => {
-              if (err) throw err;
-              console.log('The file has been saved!');
+request(options, function(error, response, body){
+    if(error) throw new Error(error);
 
-              process.exit();
-          });
+    fs.writeFile('./places_output.json', (body), (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+        process.exit();
+    });
 });
