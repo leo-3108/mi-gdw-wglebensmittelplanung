286e078cd4cb5b04c64125d609e8df0c30be05c5
Finn Nils Gedrath <finn_nils.gedrath@smail.th-koeln.de>
2019-12-16 12:11:59 +0100

    Added Test-Data

--- App.js
+++ App.js
@@ -16,6 +16,23 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/wg', function(req, res){
+    var json = {
+        name: "Test-WG",
+        adresse: {
+            Strasse: "Im Merheimer Felde",
+            Hausnummer: "14a",
+            PLZ: "51067",
+            Stadt: "Köln",
+            Land: "Deutschland"
+        },
+        Telefonnummer: 1230940218
+    };
+
+    res.contentType("application/json");
+    res.send(JSON.stringify(json))
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
