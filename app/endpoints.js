445b837bb943c058f6ec84f5daf0def1a87ac903
Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>
2020-01-06 11:51:13 +0100

    update endpoints.js

--- app/endpoints.js
+++ app/endpoints.js
@@ -4,20 +4,9 @@ exports.create = (app) => {
     });
 
     app.get('/wg', function(req, res){
-        var json = {
-            name: "Test-WG",
-            adresse: {
-                Strasse: "Im Merheimer Felde",
-                Hausnummer: "14a",
-                PLZ: "51067",
-                Stadt: "Köln",
-                Land: "Deutschland"
-            },
-            Telefonnummer: 1230940218
-        };
 
         res.contentType("application/json");
-        res.send(JSON.stringify(json))
+        res.send(JSON.stringify([]))
     });
 
 }
