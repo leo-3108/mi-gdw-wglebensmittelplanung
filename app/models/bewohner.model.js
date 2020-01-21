3893dcd27bb168ff066ab69e6166384e19fb1baf
Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>
2020-01-21 14:19:32 +0100

    added Logs

--- app/models/bewohner.model.js
+++ app/models/bewohner.model.js
@@ -45,9 +45,6 @@ exports.readall = (collection, wg_id) => {
 
 exports.readone = (collection, id, id_liste) => {
 
-    // Log
-    console.log('> Read item #', id, ' #', id_liste, ' from ', collection.collectionName);
-
     const items = collection.find({
         id: parseInt(id),
         id_liste: parseInt(id_liste)
@@ -58,26 +55,31 @@ exports.readone = (collection, id, id_liste) => {
         delete items[0]._id;
     }
 
+    // Log
+    console.log('[Log] Read Bewohner',parseInt(id),'of WG',parseInt(wg_id));
+
     return items;
 }
 
 exports.update = (collection, id, id_liste, data) => {
-    // Log (Muss evtl. noch angepasst werden)
-    console.log('> Editing item #', data.id_liste, ' from ', collection.collectionName, ' to be: ', data);
 
     return collection.update({
         id: parseInt(id),
         id_liste: parseInt(id_liste),
         data
     })
+
+    // Log
+    console.log('[Log] Update Bewohner',parseInt(id),'of WG',parseInt(wg_id));
 }
 
 exports.delete = (collection, id, id_liste) => {
-    // Log
-    console.log('> Editing item #', id_liste, ' from ', collection.collectionName);
 
     return collection.remove({
         id: parseInt(id),
         id_liste: parseInt(id_liste)
     });
+
+    // Log
+    console.log('[Log] Delete Bewohner',parseInt(id),'of WG',parseInt(wg_id));
 }
