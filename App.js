93ca9595729d6000e4991f81053961325c07e019
Finn Nils Gedrath <fgedrath@th-koeln.de>
2020-01-23 17:47:44 +0100

    Add comments

--- App.js
+++ App.js
@@ -9,13 +9,13 @@
  * @author Lining Bao, Finn Nils Gedrath, Leonard Pelzer
  */
 
-const express = require('express');
-const app = express();
-const storage = require('./app/storage');
-const endpoints = require('./app/endpoints');
-const rs = require('./app/testdata');
-const readline = require('readline');
-const rl = readline.createInterface(process.stdin,process.stdout);
+const express = require('express')
+const app = express()
+const storage = require('./app/storage')
+const endpoints = require('./app/endpoints')
+const rs = require('./app/testdata')
+const readline = require('readline')
+const rl = readline.createInterface(process.stdin,process.stdout)
 
 // Support for JSON-Body in HTTP-Request
 app.use(express.json())
@@ -38,19 +38,19 @@ app.use(function(req, res, next){
 })
 
 // Create Database
-const db = storage.init();
+const db = storage.init()
 
 // Create Endpoints
-endpoints.create(app, storage, db);
+endpoints.create(app, db)
 
 
 app.listen(3000, '0.0.0.0', function () {
-  rl.question("0: Testdaten; 1: leerer Datensatz\n", function (answer){
+  rl.question("0: Testdaten 1: leerer Datensatz\n", function (answer){
     let tmp=answer
 
     if(tmp==0) rs.reset()
     else rs.resetclear()
-    console.log('WG-Lebensmittelplaner listening on port 3000 on any IPv4-Adress!');
+    console.log('WG-Lebensmittelplaner listening on port 3000 on any IPv4-Adress!')
     rl.close
-  });
-});
+  })
+})
